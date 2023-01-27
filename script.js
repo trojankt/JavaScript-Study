23d62662ac033bc657e9d46f0cb18e8dc2a2c4ef
trojankt <92526460+trojankt@users.noreply.github.com>
2023-01-27 18:42:43 +0100

    copy text from another starter file

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let js = "amazing";
+console.log(     40 + 8 + 23 - 10);
+console.log('Trojan');
+console.log(23);
+let firstName = 'Krystian';
+console.log(firstName);
+let fisrt_name_person = 'Krystek'; 
+console.log(fisrt_name_person);
+
+let isIsland= 'Poland';
+let population = 1000000;
+let continent ='Europe';
+let language ='';
+console.log(isIsland, population, continent, language );
