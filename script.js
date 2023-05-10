38523f49c0bacf8d15a71ceb970ec9fee6852c55
trojankt <92526460+trojankt@users.noreply.github.com>
2023-05-10 21:41:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,4 @@
-/* komentarz wierszowy za pomocą gwiazdki 
-let js = "amazing";
-console.log(     40 + 8 + 23 - 10);
-console.log('Trojan');
-console.log(23);
-let firstName = 'Krystian';
-console.log(firstName);
-let fisrt_name_person = 'Krystek'; 
-console.log(fisrt_name_person);
 
-let isIsland= 'Poland';
-let population = 1000000;
-let continent ='Europe';
-let language ='';
-console.log(isIsland, population, continent, language );
-console.log(     40 + 8 + 23 - 10);
-*/
-
-/*
-//*****boolean variable*****
-true;
-
-let javascriptItFun = true;
-console.log(javascriptItFun);
-
-console.log(typeof javascriptItFun);
-console.log(typeof 'Krystek');
-console.log(typeof 1993);
-
-javascriptItFun = "YES";
-console.log(typeof javascriptItFun);
-
-let year;
-console.log(year);
-console.log(typeof year);
-*/
-
-// *****Deklarowanie zmiennych w JS let, const & var***** 
-//  Zmiana wartośi zmiennej z jednej na drugą nazywamy "ZMUTOWANIEM" (mutanted)
 
 const age = 16
 const old = age >= 18;
